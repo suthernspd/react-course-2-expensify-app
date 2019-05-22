27861a891fc0aa6241af170501e79b94d18390fb
Sutherns <suthernspd@gmail.com>
2019-05-22 15:52:38 +0100

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,60 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+export { firebase, db as default };
+
+// // child_removed
+// db.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// db.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// db.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// /* db.ref().set({
+//     name: 'Peter Sutherns',
+//     age: 25,
+//     location: {
+//         city: 'England',
+//         country: 'The World'
+//     }
+// }).then(() => {
+//     console.log('Date is saved');
+// }).catch((e) => {
+//     console.log('This failed.', e);
+// });
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e);
+//     }); */
+// /* 
+// db.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }); */

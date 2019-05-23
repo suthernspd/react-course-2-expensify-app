87df41221a810e7c86bc0617e12e1ffc904c47e9
Sutherns <suthernspd@gmail.com>
2019-05-23 09:55:30 +0100

    Set up loading expense data from firebase

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -16,5 +17,8 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
+ReactDOM.render(<p>Loading...</p>,document.getElementById('app'));
 
-ReactDOM.render(jsx,document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx,document.getElementById('app'));
+});
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -65,3 +65,12 @@ test('should NOT edit an expense with invalid id', () => {
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});

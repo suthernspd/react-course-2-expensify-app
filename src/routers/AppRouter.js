a1232f11a67a1b8166c89dcba0f06188c2c4699a
Sutherns <suthernspd@gmail.com>
2019-05-24 10:20:13 +0100

    updated more modules

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from "history";
 import LoginPage from '../components/LoginPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
@@ -10,7 +10,7 @@ import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
 
